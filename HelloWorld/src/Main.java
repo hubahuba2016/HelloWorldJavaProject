2d72e3e8d6b958d1f94ef36e30c7f0b7d2d51354
Hubaka Ghayati Setyakurnia <hubakag@gmail.com>
2023-04-03 19:33:26 +1300

    first_commit

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static String name = "Hubaka";
+    int age = 25;
+    static String location = "Bandung, West Java, Indonesia";
+    public static void main(String[] args) {
+        System.out.print("Hello world!" + " My name is "+name+", i am from "+location+"!");
+
+    }
+}
