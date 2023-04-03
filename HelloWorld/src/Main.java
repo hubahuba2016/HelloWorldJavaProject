af12e43bd10a01aa6cc90de329ae4d723c1012c9
Hubaka Ghayati Setyakurnia <hubakag@gmail.com>
2023-04-03 19:44:47 +1300

    Update Main.java

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static String name = "Hubaka";
-    int age = 25;
-    static String location = "Bandung, West Java, Indonesia";
+    public static int age = 25;
+    public static String location = "Bandung, West Java, Indonesia";
     public static void main(String[] args) {
-        System.out.print("Hello world!" + " My name is "+name+", i am from "+location+"!");
+        System.out.print("Hello world!" + " My name is "+name+", i am from "+location+"!"+" Oh yeah, i am "+age+" years old.");
 
     }
 }
